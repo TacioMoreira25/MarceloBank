485bc8f40df813228182d88dc1c885f49ed7ddc7
tacio <moreiravini144@gmail.com>
2025-10-03 16:39:52 -0300

    Initial commit

---
+++ src/main/java/com/MarceloBank/MarceloBank/MarceloBankApplication.java
@@ -0,0 +1,13 @@
+package com.MarceloBank.MarceloBank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MarceloBankApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MarceloBankApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/MarceloBank/MarceloBank/MarceloBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.MarceloBank.MarceloBank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MarceloBankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
