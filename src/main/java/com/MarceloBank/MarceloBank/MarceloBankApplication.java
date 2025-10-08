76665a24d5680e73838926de67120224c478e310
tacio <moreiravini144@gmail.com>
2025-10-08 19:26:59 -0300

    feat: implement CORS configuration for API access

--- src/main/java/com/MarceloBank/MarceloBank/MarceloBankApplication.java
+++ src/main/java/com/MarceloBank/MarceloBank/MarceloBankApplication.java
@@ -1,20 +1,12 @@
 package com.MarceloBank.MarceloBank;
 
-import com.MarceloBank.MarceloBank.ui.MenuPrincipal;
 import io.github.cdimascio.dotenv.Dotenv;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class MarceloBankApplication implements CommandLineRunner {
-
-    private final MenuPrincipal menuPrincipal;
-
-    public MarceloBankApplication(MenuPrincipal menuPrincipal) {
-        this.menuPrincipal = menuPrincipal;
-    }
-
+public class MarceloBankApplication
+{
     public static void main(String[] args)
     {
         Dotenv dotenv = Dotenv.configure().ignoreIfMissing().load();
@@ -23,10 +15,5 @@ public class MarceloBankApplication implements CommandLineRunner {
                 System.setProperty(e.getKey(), e.getValue())
         );
         SpringApplication.run(MarceloBankApplication.class, args);
-    }
-
-    @Override
-    public void run(String... args) throws Exception {
-        menuPrincipal.exibir();
     }
 }
